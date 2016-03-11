4a30363965672bb9ab5fd6a301396e37b22a45f3
Ilya Dvoretski <ilya_dv@yahoo.com>
2016-03-11 06:36:37 +0200

    LearningActivity optimizations

--- mobile/src/main/java/spaced/com/spaced/LearningActivity.java
+++ mobile/src/main/java/spaced/com/spaced/LearningActivity.java
@@ -1,8 +1,15 @@
 package spaced.com.spaced;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import models.CardModel;
@@ -14,9 +21,22 @@ import models.DecksManager;
  */
 public class LearningActivity extends AppCompatActivity {
 
+    TextView questionTextView;
+    TextView answerTextView;
+    ImageView image1;
+    ImageView image2;
+    TextView skipButton;
+    TextView previousButton;
+    TextView answerButton;
+
     public final static String DECK_ID = "deckId";
     private DeckModel mDeck;
     private CardModel mCurrentCard;
+    private int cardIndex = 0;
+
+    private void setAppTitle(){
+        this.setTitle(mDeck.getDeckName() + " ("+ (cardIndex + 1) + "/" + mDeck.getCardsQuantity() + ")");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +44,110 @@ public class LearningActivity extends AppCompatActivity {
         setContentView(R.layout.card_with_question_activity);
 
         Intent intent = getIntent();
-        mDeck = DecksManager.getInstance().getLocalDeckById((int)intent.getExtras().get(DECK_ID));
-        mCurrentCard = mDeck.getmCards().get(0);
+        mDeck = DecksManager.getInstance().getLocalDeckById((int) intent.getExtras().get(DECK_ID));
+        mCurrentCard = mDeck.getmCards().get(cardIndex);
+        questionTextView = (TextView) findViewById(R.id.questionText);
+        answerTextView = (TextView) findViewById(R.id.answerTextView);
+        image1 = (ImageView)findViewById(R.id.cardImage);
+        image2 = (ImageView)findViewById(R.id.vintik);
+        answerButton = (TextView) findViewById(R.id.showAnswerButton);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/UbuntuCondensedRegular.ttf");
+        skipButton = (TextView)findViewById(R.id.skipButton);
+        previousButton = (TextView)findViewById(R.id.previousButton);
+        questionTextView.setTypeface(font);
+        answerButton.setTypeface(font);
+        answerTextView.setTypeface(font);
+        skipButton.setTypeface(font);
+        previousButton.setTypeface(font);
 
-        ((TextView)findViewById(R.id.questionText)).setText(mCurrentCard.getCardQuestion());
+        questionTextView.setText(mCurrentCard.getCardQuestion());
 
+        ImageView shesteryonok = (ImageView) findViewById(R.id.vintik);
+        Animation rotation = new RotateAnimation(0f, 359, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotation.setDuration(5000);
+        rotation.setInterpolator(new LinearInterpolator());
+        rotation.setRepeatCount(Animation.INFINITE);
+        shesteryonok.startAnimation(rotation);
+
+        setAppTitle();
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDeck.getmCards().size() > cardIndex + 1) {
+                    mCurrentCard = mDeck.getmCards().get(++cardIndex);
+                    questionTextView.setText(mCurrentCard.getCardQuestion());
+                    setAppTitle();
+                }
+            }
+        });
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cardIndex > 0) {
+                    mCurrentCard = mDeck.getmCards().get(--cardIndex);
+                    questionTextView.setText(mCurrentCard.getCardQuestion());
+                    setAppTitle();
+                }
+            }
+        });
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View imageContainer = findViewById(R.id.imageLayoutView);
+                Animation animation = new ScaleAnimation(1.0f, 0f, 1.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                animation.setDuration(500);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        image1.setVisibility(View.GONE);
+                        image2.setVisibility(View.GONE);
+                        answerTextView.setText(mCurrentCard.getCardAnswer());
+                        answerTextView.setVisibility(View.VISIBLE);
+                        Animation animationGrow = new ScaleAnimation(0f, 1f, 1.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                        animationGrow.setDuration(500);
+                        imageContainer.startAnimation(animationGrow);
+                    }
+                });
+                imageContainer.startAnimation(animation);
+            }
+        });
+    }
+
+    private void showQuestion(){
+        questionTextView.setText(mCurrentCard.getCardQuestion());
+        final View imageContainer = findViewById(R.id.imageLayoutView);
+        Animation animation = new ScaleAnimation(1.0f, 0f, 1.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setDuration(500);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                image1.setVisibility(View.VISIBLE);
+                image2.setVisibility(View.VISIBLE);
+                answerTextView.setVisibility(View.GONE);
+                Animation animationGrow = new ScaleAnimation(0f, 1f, 1.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                animationGrow.setDuration(500);
+                imageContainer.startAnimation(animationGrow);
+            }
+        });
+        imageContainer.startAnimation(animation);
     }
 }
